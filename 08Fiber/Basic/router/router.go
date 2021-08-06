0b4e423b6eab25ba9e743f0f860f4c96ae654ee4
techmaster <cuong@techmaster.vn>
2021-08-06 07:44:20 +0700

    Huong dan log loi

--- 08Fiber/Basic/router/router.go
+++ 08Fiber/Basic/router/router.go
@@ -2,31 +2,69 @@ package router
 
 import (
 	"demofiber/controller"
+	"fmt"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func RegisterRoutes(app *fiber.App) {
 	app.Get("/", controller.Homepage)
+	routeDemoErrorHandling(app)
+	routeSession(app)
+	demoParamQuery(app)
+}
+func routeDemoErrorHandling(app *fiber.App) {
+	app.Get("/err/error", controller.GenericError) // Trả về lỗi trên màn hình
 
-	app.Get("/error", controller.GenericError) // Trả về lỗi trên màn hình
+	app.Get("/err/login", controller.DemoUnAuthorized) //Trả về lỗi JSON
 
-	app.Get("/login", controller.DemoUnAuthorized) //Trả về lỗi JSON
+	app.Get("/err/apierror", controller.DemoRESTAPIError) //Trả về lỗi JSON
 
-	app.Get("/apierror", controller.DemoRESTAPIError) //Trả về lỗi JSON
+	app.Get("/err/dividezero", controller.DivideZero) //Trả về custom Error page
 
-	app.Get("/dividezero", controller.DivideZero) //Trả về custom Error page
+	app.Get("/err/posts", controller.GetAllPosts) //Trả về custom Error page
 
-	app.Get("/posts", controller.GetAllPosts) //Trả về custom Error page
+	app.Get("/err/page", controller.DemoPage) //Trả về custom Error page
 
-	app.Get("/page", controller.DemoPage) //Trả về custom Error page
+	app.Get("/err/panic", controller.DemoPanicError) //Xử lý lỗi panic error
 
-	app.Get("/panic", controller.DemoPanicError) //Xử lý lỗi panic error
+	app.Get("/err/warn", controller.DemoWarning) //Xử lý Warning
 
-	app.Get("/warn", controller.DemoWarning) //Xử lý Warning
-	app.Get("/cause", controller.DemoCause)
+	app.Get("/err/cause", controller.DemoCause)
+}
 
-	app.Get("/getsessionid", controller.GetSessionID)
+func demoParamQuery(app *fiber.App) {
+	app.Get("/para/author/:name/book/:id", func(c *fiber.Ctx) error {
+		if id, err := c.ParamsInt("id"); err == nil {
+			fmt.Fprintf(c, "%d\n", id)
+			return c.SendString(fmt.Sprintf("%s - %d", c.Params("name"), id))
+		} else {
+			return c.SendString(fmt.Sprintf("%s - %s", c.Params("name"), c.Params("id")))
+		}
+	})
 
-	app.Get("/counter", controller.SessionCounter)
+	// Plus - greedy - not optional
+	app.Get("/para/user/+", func(c *fiber.Ctx) error {
+		return c.SendString(c.Params("+"))
+	})
+
+	// Optional parameter
+	app.Get("/para/foo/:name?", func(c *fiber.Ctx) error {
+		return c.SendString(c.Params("name"))
+	})
+
+	// Wildcard - greedy - optional
+	app.Get("/para/bar/*", func(c *fiber.Ctx) error {
+		return c.SendString(c.Params("*"))
+	})
+
+	// This route path will match requests to "/para/name:customVerb", since the parameter character is escaped
+	app.Get("/para/name\\:customVerb", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, Community")
+	})
+}
+
+func routeSession(app *fiber.App) {
+	app.Get("/session/getsessionid", controller.GetSessionID)
+	app.Get("/session/counter", controller.SessionCounter)
 }
